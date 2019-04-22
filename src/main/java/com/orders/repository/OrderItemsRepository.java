a81d4da623e1ba5ad9cd79f10c4be689a7dc8418
pedroPolonea <phpolonea@gmail.com>
2019-04-22 00:01:59 -0300

    Ajustando consulta.

--- src/main/java/com/orders/repository/OrderItemsRepository.java
+++ src/main/java/com/orders/repository/OrderItemsRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Repository
 public interface OrderItemsRepository extends JpaRepository<OrderItemsEntity, Long> {
 
-    List<OrderItemsEntity> findByOrderAsc(OrderEntity orderEntity);
+    List<OrderItemsEntity> findByOrder(OrderEntity orderEntity);
 }
